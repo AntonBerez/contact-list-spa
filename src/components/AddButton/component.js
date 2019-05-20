d20221fed8ab575183e465fa64ddb3449da556ea
Anton Berezenskyi <ant.vla.ber@gmail.com>
2019-05-20 21:38:47 +0300

    added styles to components

--- src/components/AddButton/component.js
+++ src/components/AddButton/component.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import './component.scss';
-import { Link } from "react-router-dom";
+import './component.scss';
+import { Link } from 'react-router-dom';
 
 function AddButton() {
   return (
     <React.Fragment>
-      <Link to='/create'>Add</Link>
+      <Link to='/create' className='create-link'>Add Contact</Link>
     </React.Fragment>
   );
 }
